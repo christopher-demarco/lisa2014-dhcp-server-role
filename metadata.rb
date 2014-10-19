11aa073cbe295dd3693aabaf91976d61521a17bf
Christopher DeMarco <demarco@maya.com>
2014-10-19 12:48:24 -0400

    dhcp cookbook dependency in metadata

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,9 @@
 name             'dhcp-server-role'
-maintainer       'The Authors'
-maintainer_email 'you@example.com'
+maintainer       'Christopher DeMarco'
+maintainer_email 'cdemarco@gmail.com'
 license          'all_rights'
 description      'Installs/Configures dhcp-server-role'
 long_description 'Installs/Configures dhcp-server-role'
 version          '0.1.0'
 
+depends "dhcp"
